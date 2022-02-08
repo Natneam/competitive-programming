1baa75548a875e33910bbbf0b672496011223301
Natneam Mesele <58377289+Natneam@users.noreply.github.com>
2022-02-08 07:23:25 +0300

    Time: 36 ms (68.31%), Space: 13.8 MB (91.66%) - LeetHub

--- 258-add-digits/258-add-digits.py
+++ 258-add-digits/258-add-digits.py
@@ -1,11 +1,8 @@
 class Solution:
-    def addDigits(self, num: int) -> int:
-        curr_num = str(num)
-        while len(curr_num) > 1:
-            x = 0
-            for i in curr_num:
-                x += int(i)
-            curr_num = str(x)
-                
-        
-        return int(curr_num)
+    def addDigits(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n % 9 == 0:
+            return 9
+        else:
+            return n % 9
