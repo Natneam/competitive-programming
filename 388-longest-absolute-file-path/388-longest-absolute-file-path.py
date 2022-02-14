d242c804b2255240fcc69ddda39589df483ce3dc
Natneam Mesele <58377289+Natneam@users.noreply.github.com>
2022-02-14 11:38:05 +0300

    Time: 42 ms (42.73%), Space: 13.9 MB (95.04%) - LeetHub

--- 388-longest-absolute-file-path/388-longest-absolute-file-path.py
+++ 388-longest-absolute-file-path/388-longest-absolute-file-path.py
@@ -1,13 +1,18 @@
 class Solution:
     def lengthLongestPath(self, i: str) -> int:
         max_length = 0
-        path_length = {0 : 0}
+        path = i.split('\n')
+        stack = [(0, -1)] #[(path_length, depth), ...] 
         
-        for path in i.split('\n'):
-            depth = path.count('\t')
-            if '.' in path:
-                max_length = max(max_length, path_length[depth] + len(path) - depth)
+        for p in path:
+            depth = p.count("\t")
+            length = len(p) - depth
+            while stack and stack[-1][1] >= depth:
+                    stack.pop()
+                    
+            if '.' in p:
+                max_length = max(max_length, length + stack[-1][0])
             else:
-                path_length[depth + 1] = path_length[depth] + len(path) - depth + 1
+                stack.append((stack[-1][0] + length + 1, depth))
         
         return max_length
