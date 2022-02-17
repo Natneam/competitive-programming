a1468677b947a513f2d3fa8f4c0eda15f4b9433e
Natneam Mesele <58377289+Natneam@users.noreply.github.com>
2022-02-17 16:59:29 +0300

    Time: 127 ms (27.38%), Space: 14 MB (89.03%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,27 +1,49 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
         #bfs
-        queue = deque([(sr, sc)])
+        # queue = deque([(sr, sc)])
+
+        # original_color = image[sr][sc]
+
+        # if newColor == original_color:
+        #     return image
+
+        # def is_valid(i, j):
+        #     row = len(image)
+        #     col = len(image[0])
+        #     return 0 <= i < row and 0 <= j < col and image[i][j] == original_color
+
+        # while queue:
+        #     index_x, index_y = queue.popleft()
+
+        #     image[index_x][index_y] = newColor
+
+        #     directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+        #     for dir_x, dir_y in directions:
+        #         x, y = index_x + dir_x, index_y + dir_y
+        #         if is_valid(x, y):
+        #             queue.append((x, y))
+        # return image
         
-        original_color = image[sr][sc]
+        #dfs
         
-        if newColor == original_color:
-            return image
-        
-        def is_valid(i, j):
+        def is_valid(image, i, j, original_color):
             row = len(image)
             col = len(image[0])
-            return 0 <= i < row and 0 <= j < col and image[x][y] == original_color
+            return 0 <= i < row and 0 <= j < col and image[i][j] == original_color
         
-        while queue:
-            index_x, index_y = queue.popleft()
-            
-            image[index_x][index_y] = newColor
+        def dfs(image, visited, indexes, original_color, new_color):
+            index_x, index_y =indexes[0], indexes[1]
+            image[index_x][index_y] = new_color
             
             directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
             
-            for dir_x, dir_y in directions:
-                x, y = index_x + dir_x, index_y + dir_y
-                if is_valid(x, y):
-                    queue.append((x, y))
+            for x, y in directions:
+                new_x , new_y = index_x + x, index_y + y
+                if is_valid(image, new_x, new_y, original_color) and (new_x, new_y) not in visited:
+                    visited.add((new_x, new_y))
+                    dfs(image, visited, (new_x, new_y), original_color, new_color)
+            
+        dfs(image, set([(sr, sc)]), (sr, sc), image[sr][sc], newColor)
         return image
