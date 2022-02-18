73d0b9526c5d8ed9adcaf1e988f46732a0e34839
Natneam Mesele <58377289+Natneam@users.noreply.github.com>
2022-02-18 10:08:08 +0300

    Time: 4117 ms (5.00%), Space: 42.1 MB (8.66%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,13 +1,27 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maximum = -float('inf')
-        previous = -float('inf')
-        
-        for num in nums:
-            if num + previous > num:
-                previous = num + previous
-            else:
-                previous = num
-            maximum = max(maximum, previous)
-        
-        return maximum
+        def solve(arr, start, end):
+            if start > end:
+                return float('-inf')
+            
+            mid = start + (end - start) // 2
+            left_sum = 0
+            right_sum = 0
+            
+            # find the maximum left subbary sum which includes the element at [mid - 1]
+            curr_sum = 0
+            
+            for i in range(mid - 1, start - 1, -1):
+                curr_sum += nums[i]
+                left_sum = max(left_sum, curr_sum)           
+            
+            # find the maximum right subbary sum whihc includes the element at [mid + 1]
+            curr_sum = 0
+            
+            for i in range(mid+1, end+1):
+                curr_sum += nums[i]
+                right_sum = max(right_sum, curr_sum)
+            
+            
+            return max(right_sum + nums[mid] + left_sum, solve(arr, mid+1, end), solve(arr, start, mid-1))
+        return solve(nums, 0, len(nums) - 1)
