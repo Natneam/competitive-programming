d281b38daff1806308aeb9c9525fe1eca426be82
Natneam Mesele <58377289+Natneam@users.noreply.github.com>
2022-02-28 07:52:11 +0300

    Time: 50 ms (31.39%), Space: 13.8 MB (88.25%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.py
+++ 228-summary-ranges/228-summary-ranges.py
@@ -3,21 +3,20 @@ class Solution:
         if len(nums) == 0:
             return []
         
-        buffer = []
-        curr = [nums[0], 0]
-        nums.append(nums[-1] + 10)
+        ans = []
+        _range = [nums[0], 0]
+        nums.append(nums[-1] + 10)  #padding
         
         for i in range(1, len(nums)):
             if nums[i] - 1 != nums[i-1]:
-                curr[1] = nums[i-1]
-                buffer.append(curr)
-                curr = [nums[i], 0]
-        
-        ans = []
-        for item in buffer:
-            if item[0] == item[1]:
-                ans.append(str(item[0]))
-            else:
-                ans.append(f"{item[0]}->{item[1]}")
+                _range[1] = nums[i-1]
+                
+                if _range[0] != _range[1]:
+                    ans.append(f"{_range[0]}->{_range[1]}")
+                else:
+                    ans.append(str(_range[0]))
+                
+                _range = [nums[i], 0]
+                
         return ans
         
