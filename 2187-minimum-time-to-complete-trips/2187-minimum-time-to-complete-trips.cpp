473233069e0fc10552a2544507781e608c85d037
Natneam Mesele <58377289+Natneam@users.noreply.github.com>
2022-03-18 22:07:59 +0300

    Time: 421 ms (60.44%), Space: 249 MB (5.00%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    long long check(vector<int> time, long long mid){
+        long long a = 0;
+        
+        for(auto t : time){
+            a += mid / t;
+        }
+        
+        return a;
+    }
+    
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        
+        long long maximum = time[0];
+        
+        for(int i=1; i < time.size(); i++){
+            if (time[i] > maximum){
+                maximum = time[i];
+            }
+        }
+        
+        long long start = 1;
+        long long end = totalTrips * maximum;
+        long long ans = totalTrips * maximum;
+        
+        while(start <= end){
+            long long mid = start + (end - start)/ 2;
+            long long value = check(time, mid);
+            
+            if (value < totalTrips){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+                if (mid < ans){
+                    ans = mid;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
