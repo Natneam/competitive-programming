3484c25e590569519c364b28b43e88cfe3c348f9
Natneam Mesele <58377289+Natneam@users.noreply.github.com>
2022-03-24 09:18:38 +0300

    Time: 142 ms (21.80%), Space: 42 MB (57.42%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.rbegin(), people.rend());
+        int i, j;
+        
+        for(i = 0, j = people.size() - 1; i <= j; i ++){
+            if (people[i] + people[j] <= limit) j--;
+        }
+        
+        return i;
+    }
+};
