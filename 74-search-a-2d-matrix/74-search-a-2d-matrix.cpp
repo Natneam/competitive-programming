5655d8d865957695eee46a4ba4a3653685ee5400
Natneam Mesele <58377289+Natneam@users.noreply.github.com>
2022-03-30 08:39:07 +0300

    Time: 4 ms (77.73%), Space: 9.6 MB (5.69%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
-    bool searchRow(vector<vector<int>>& matrix, int row, int target){
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // search the column the to find the largest number which is less than or equals the target
+        // then search this row to find the exact target
+        
+        //search the column
         int start{0};
-        int end = matrix[row].size() - 1;
-        int mid {};
-
+        int end = matrix.size() - 1;
+        int bestSoFar{};
+        int mid{};
+        
+        while(start <= end){
+            mid = start + (end - start)/2;
+            if(matrix[mid][0] <= target){
+                bestSoFar = mid;
+                start = mid + 1;
+            } else end = mid - 1;
+        }
+        
+        
+        //search the row
+        
+        start = 0;
+        end = matrix[bestSoFar].size() - 1;
+        mid = 0;
+        
         while(start <= end){
             mid = start + (end - start) / 2;
-            if(matrix[row][mid] == target) return true;
-            else if(matrix[row][mid] < target) start = mid + 1;
+            if(matrix[bestSoFar][mid] == target) return true;
+            else if (matrix[bestSoFar][mid] < target) start = mid + 1;
             else end = mid - 1;
         }
         
         return false;
-    }
-    
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        //nlog(m) - binary search each row
-        for(int i=0; i < matrix.size(); i++){
-            if(searchRow(matrix, i, target)) return true;
-        }
-        return false;
+        
     }
 };
