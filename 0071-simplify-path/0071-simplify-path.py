9dbf48ec56419ba93109c7901d69d687c83e9466
Natneam Mesele <58377289+Natneam@users.noreply.github.com>
2023-02-03 03:16:43 -0500

    Time: 40 ms (51.84%), Space: 13.8 MB (76.11%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,10 +1,17 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
+        path, i = path.split("/"), 0
+        
+        for j in range(len(path)):
+            if path[j] != "":
+                path[i] = path[j]
+                i += 1
+        
         stack = []
-        for p in path.split("/"):
+        for p in path[:i]:
             if p == "..":
                 if stack: stack.pop()
-            elif p not in [".", "..", ""]:
+            elif p not in ["", "."]:
                 stack.append(p)
         
         return "/" + "/".join(stack)
