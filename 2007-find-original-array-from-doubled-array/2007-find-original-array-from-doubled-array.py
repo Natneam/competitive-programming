dbb00e802208e30291f9906e5a575ba6a916bdda
Natneam Mesele <58377289+Natneam@users.noreply.github.com>
2023-02-18 20:20:56 +0300

    Time: 1664 ms (30.54%), Space: 32.8 MB (35.25%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -3,29 +3,23 @@ class Solution:
         counter = Counter(changed)
         changed.sort()
         res = []
+        if counter[0] % 2: return []
         
         for c in changed:
             if c not in counter:
                 continue
             
-            if c == 0:
-                if counter[c] < 2:
-                    return []
-                counter[c] -= 2
-                res.append(c)
-                
-            elif c*2 in counter:
+            if c*2 in counter:
                 res.append(c)
                 counter[c] -= 1
                 counter[c*2] -= 1
                 
+                if counter[c] == 0:
+                    del counter[c]
                 if counter[c*2] == 0:
                     del counter[c*2]
-                    
             else:
                 return []
-            
-            if counter[c] == 0:
-                del counter[c]
+
         
         return res
