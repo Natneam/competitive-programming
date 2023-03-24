9e797c86a75b9a7f3bac04e1749f4f23044c6f94
Natneam Mesele <58377289+Natneam@users.noreply.github.com>
2023-03-24 15:19:36 +0300

    Time: 378 ms (27.70%), Space: 21.9 MB (19.32%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if (i, j) not in visited and grid[i][j] == "1":
-                    self.dfs(grid, visited, i, j)
+                    self.bfs(grid, visited, i, j)
                     res += 1
         return res
                     
@@ -18,6 +18,19 @@ class Solution:
             next_i, next_j = i + x, j + y
             self.isvalid(next_i, next_j, grid, visited) and self.dfs(grid, visited, next_i, next_j)
     
+    def bfs(self, grid, visited, i, j):
+        queue = deque([(i, j)])
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        visited.add((i, j))
+        while queue:
+            i, j = queue.popleft()
+            for x, y in directions:
+                next_i, next_j = i + x, j + y
+                if self.isvalid(next_i, next_j, grid, visited):
+                    visited.add((next_i, next_j))
+                    queue.append((next_i, next_j))
+        
+        
     def isvalid(self, i, j, grid, visited):
         return 0 <= i < len(grid) and 0 <= j < len(grid[0]) and \
                 (i, j) not in visited and grid[i][j] == "1"
